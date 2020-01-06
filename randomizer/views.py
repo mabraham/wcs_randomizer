45dfa45b3bf856b75503f1ade7a7d838f5113d2c
Mark Abraham <mark.j.abraham@gmail.com>
2020-01-06 23:50:02 +0100

    Spotlight names working

--- randomizer/views.py
+++ randomizer/views.py
@@ -1,8 +1,10 @@
-import random
+from django.core import serializers
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.generic import View
+import json
+import random
 from randomizer.models import SpotlightDancer
 
 class DisplayView(View):
@@ -25,22 +27,30 @@ class SpotlightView(View):
         context = {'title': 'Spotlight randomizer'}
         return render(request, 'spotlight.html', context)
 
+from django.forms.models import model_to_dict
+
 class SpotlightDancerView(View):
 
     def get(self, request, *args, **kwargs):
         spotlight_dancers_who_have_not_danced = SpotlightDancer.objects.filter(has_danced__exact=False)
         number_of_dancers_remaining = len(spotlight_dancers_who_have_not_danced)
-        choice = ''
         if number_of_dancers_remaining > 0:
             bib_number = random.choice(spotlight_dancers_who_have_not_danced).bib_number
             choice = str(bib_number)
 
             # Now set the flag to show that this one has danced
-            that_dancer = SpotlightDancer.objects.get(bib_number__exact=bib_number)
+            that_dancer_set = SpotlightDancer.objects.filter(bib_number__exact=bib_number)
+            that_dancer = that_dancer_set[0]
             that_dancer.has_danced = True
             that_dancer.save()
 
-        return JsonResponse(choice, safe=False)
+        else:
+            that_dancer = SpotlightDancer(name="", bib_number="   ", has_danced=False, id="")
+
+        dict_obj = model_to_dict(that_dancer)
+        response = JsonResponse(dict_obj, safe=False)
+
+        return response
 
 class SpotlightMCView(View):
 
